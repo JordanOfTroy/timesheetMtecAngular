661876f5298b3570c1f1b6fddb516b1074c2c2b1
Jordan <jordan@jordans-air.lan>
2023-09-05 19:36:16 -0600

    unit 7 pt 2

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Department } from 'src/app/interfaces/department';
+import { Employee } from 'src/app/interfaces/employee';
 import { DepartmentsService } from 'src/app/services/departments.service';
 
 @Component({
@@ -14,6 +15,8 @@ export class TimesheetComponent implements OnInit {
   departments: Department[] | undefined
   department: Department | undefined
   employeeNameFC = new FormControl('')
+  employees: Employee[] = [];
+  employeeId = 0;
 
   constructor(
     private route: ActivatedRoute,
@@ -23,6 +26,21 @@ export class TimesheetComponent implements OnInit {
   ngOnInit(): void {
     this.departments = this.departmentsService.departments;
     this.department = this.departments.find(department => department.id === this.route.snapshot.params['id']);
-}
+  }
+
+  addEmployee(): void {
+    if (this.employeeNameFC.value) {
+        this.employeeId++;
+
+        this.employees.push({
+            id: this.employeeId.toString(),
+            departmentId: this.department?.id,
+            name: this.employeeNameFC.value,
+            payRate: Math.floor(Math.random() * 50) + 50,
+        });
+
+        this.employeeNameFC.setValue('');
+    }
+  }
 
 }
